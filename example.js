78070a6d25702f5b81dbd39a28e2594958c47b76
DavidWells <hello@davidwells.io>
2019-12-06 16:24:18 -0800

    chore move index

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-const { LocalGit } = require("./localGit")
+const { LocalGit } = require("./src/localGit")
 /*
 const CliArgs = {
   base: string
@@ -10,15 +10,23 @@ const CliArgs = {
 }
 */
 
+/*
 const cliArgs = {
   base: "develop",
-}
+}*/
+const cliArgs = {}
 
 async function run() {
+  console.log('start')
   // const localPlatform = new LocalGit(dsl.settings.cliArgs)
   const localPlatform = new LocalGit(cliArgs)
+  console.log('localPlatform', localPlatform)
   const git = await localPlatform.getPlatformGitRepresentation()
   console.log('git', git)
+  const srcCode = git.fileMatch("**/*.js")
+  if (srcCode.edited) {
+    console.log('srcCode has been edited')
+  }
 }
 
 run()
